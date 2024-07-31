54c005fa9e3a42c61a7682adf753a57343a521f5
Kazuchika Yanagi <yanagikazuchika@gmail.com>
2024-07-31 16:50:00 -0700

    update: load to-do items button to use ajax and async await

--- script.js
+++ script.js
@@ -7,38 +7,19 @@ $(function () {
     $(this).next().slideToggle();
   });
 
-  // function getData() {
-  //   return new Promise((resolve, reject) => {
-  //     $.ajax({
-  //       url: `https://dummyjson.com/todos`,
-  //       type: `GET`,
-  //       success: function (response) {
-  //         resolve(response);
-  //         // console.log(response.todos);
-  //         // console.log($("button").next().children());
-  //         $(this).next().append("<li>hey</li>");
-  //       },
-  //       error: function (error) {
-  //         reject(error);
-  //       },
-  //     });
-  //   });
-  // }
-
   toDo.on("click", async function () {
-    try {
-      const res = await fetch("https://dummyjson.com/todos");
-      const data = await res.json();
-      const todoData = data.todos;
-      // console.log(todoData);
-
-      for (const el of todoData) {
-        toDo.find("ul").append(`<li>${el.todo}</li>`);
-      }
-
-      // console.log(toDo.find("ul"));
-    } catch (error) {
-      console.error(error);
-    }
+    $.ajax({
+      url: "https://dummyjson.com/todos",
+      success: async function (response) {
+        const res = await response;
+        const getData = res.todos;
+        for (const el of getData) {
+          toDo.find("ul").append(`<li>${el.todo}</li>`);
+        }
+      },
+      error: function (error) {
+        console.error(error);
+      },
+    });
   });
 });
